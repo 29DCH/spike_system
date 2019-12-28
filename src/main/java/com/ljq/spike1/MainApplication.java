d882478da8b6c7f3da7d22bc0fa2fa1fc3e11be4
29DCH <2265940560@qq.com>
2019-12-28 14:43:51 +0800

    Initial commit

---
+++ src/main/java/com/ljq/spike1/MainApplication.java
@@ -0,0 +1,17 @@
+package com.ljq.spike1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author :ljq
+ * @date :Created in 2019/12/26 16:25
+ * @description :启动类
+ * @creed :Talk is cheap,show me the code
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ljq/spike1/exception/GlobalException.java
@@ -0,0 +1,20 @@
+package com.ljq.spike1.exception;
+
+import com.ljq.spike1.result.CodeMsg;
+
+public class GlobalException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private CodeMsg cm;
+
+    public GlobalException(CodeMsg cm) {
+        super(cm.toString());
+        this.cm = cm;
+    }
+
+    public CodeMsg getCm() {
+        return cm;
+    }
+
+}
