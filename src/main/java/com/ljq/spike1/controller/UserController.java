15e646256b98ac08f713657dfb9e41df75c98978
29DCH <2265940560@qq.com>
2019-12-28 15:15:28 +0800

    ljq

---
+++ src/main/java/com/ljq/spike1/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.ljq.spike1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ljq.spike1.domain.MiaoshaUser;
+import com.ljq.spike1.redis.RedisService;
+import com.ljq.spike1.result.Result;
+import com.ljq.spike1.service.MiaoshaUserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	MiaoshaUserService userService;
+	
+	@Autowired
+	RedisService redisService;
+	
+    @RequestMapping("/info")
+    @ResponseBody
+    public Result<MiaoshaUser> info(Model model,MiaoshaUser user) {
+        return Result.success(user);
+    }
+    
+}
---
+++ src/main/java/com/ljq/spike1/rabbitmq/MiaoshaMessage.java
@@ -0,0 +1,20 @@
+package com.ljq.spike1.rabbitmq;
+
+import com.ljq.spike1.domain.MiaoshaUser;
+
+public class MiaoshaMessage {
+	private MiaoshaUser user;
+	private long goodsId;
+	public MiaoshaUser getUser() {
+		return user;
+	}
+	public void setUser(MiaoshaUser user) {
+		this.user = user;
+	}
+	public long getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(long goodsId) {
+		this.goodsId = goodsId;
+	}
+}
